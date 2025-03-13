f52400b2f764c3d1baadf5bd195d78a8e8907f0c
Ryan Sartor <89538678+ryanmsartor@users.noreply.github.com>
2025-03-13 00:09:04 -0500

    steps towards fixing emufresh

--- ROMs/APPS/Hide Empty Systems.sh
+++ ROMs/APPS/Hide Empty Systems.sh
@@ -1,24 +1,33 @@
 #!/bin/sh
 
+LOGFILE="/mnt/extsd/app.log"
+echo "Hiding empty systems!" > "$LOGFILE"
+
+
 ROM_DIR="/mnt/extsd/ROMs"
 EMU_DIR="/mnt/extsd/sections/emulators"
 HIDDEN_DIR="$EMU_DIR/hidden"
 
 is_empty() { # expects full dir path
 	dir="$1"
-	[ ! -d "$dir" ] && return 1
+	[ ! -d "$dir" ] && echo "no folder named $dir was found." && return 1
 
-	if ls "$dir" | grep -v -E '^\.gitkeep$' >/dev/null; then
+	for f in "$dir"/*; do
+		if [ -e "$f" ] && [ "$f" != "/path/to/directory/." ] && [ "$f" != "/path/to/directory/.." ]; then
+			echo "$dir is not empty."
+			return 1
+		fi
+		echo "$dir is empty."
 		return 0
-	else
-		return 1
-	fi
+	done
+
+
 }
 
 is_visible() { # expects basename of emu
 	emu="$1"
 
-	if [ =f "$ROM_DIR/$emu" ]; then
+	if [ -f "$ROM_DIR/$emu" ]; then
 		return 0
 	else
 		return 1
@@ -40,26 +49,29 @@ check_and_handle() { # expects basename of rom_subdir and basename of emufile
 	fi
 }
 
-mkdir -p "$HIDDEN_DIR"
+{
 
-check_and_handle "ARCADE" "00 ARCADE"
-check_and_handle "NES" "01 NES"
-check_and_handle "SNES" "02 SNES"
-check_and_handle "N64" "03 N64"
-check_and_handle "GB" "04 GB"
-check_and_handle "GBC" "05 GBC"
-check_and_handle "GBA" "06 GBA"
-check_and_handle "GW" "07 GW"
-check_and_handle "VB" "08 VB"
-check_and_handle "POKE" "09 POKE"
-check_and_handle "SMS" "10 SMS"
-check_and_handle "SMD" "11 SMD"
-check_and_handle "DC" "12 DC"
-check_and_handle "GG" "13 GG"
-check_and_handle "PS" "14 PS"
-check_and_handle "PSP" "15 PSP"
-check_and_handle "TG16" "16 TG16"
-check_and_handle "NGPC" "17 NGPC"
-check_and_handle "WSC" "18 WSC"
-check_and_handle "PICO8" "19 PICO8"
-check_and_handle "APPS" "20 APPS"
+	mkdir -p "$HIDDEN_DIR"
+	check_and_handle "ARCADE" "00 ARCADE"
+	check_and_handle "NES" "01 NES"
+	check_and_handle "SNES" "02 SNES"
+	check_and_handle "N64" "03 N64"
+	check_and_handle "GB" "04 GB"
+	check_and_handle "GBC" "05 GBC"
+	check_and_handle "GBA" "06 GBA"
+	check_and_handle "GW" "07 GW"
+	check_and_handle "VB" "08 VB"
+	check_and_handle "POKE" "09 POKE"
+	check_and_handle "SMS" "10 SMS"
+	check_and_handle "SMD" "11 SMD"
+	check_and_handle "DC" "12 DC"
+	check_and_handle "GG" "13 GG"
+	check_and_handle "PS" "14 PS"
+	check_and_handle "PSP" "15 PSP"
+	check_and_handle "TG16" "16 TG16"
+	check_and_handle "NGPC" "17 NGPC"
+	check_and_handle "WSC" "18 WSC"
+	check_and_handle "PICO8" "19 PICO8"
+	check_and_handle "APPS" "20 APPS"
+
+} >> "$LOGFILE" 2>&1
